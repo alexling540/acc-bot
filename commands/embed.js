32295da20bfcef2999a80c13e17861644231338b
Alex Ling <22086776+alexling540@users.noreply.github.com>
2020-10-07 23:17:58 -0500

    embed helper functions, save, delete

--- commands/embed.js
+++ commands/embed.js
@@ -1,6 +1,32 @@
 const { firestore, database } = require('../firestore.js');
 const Discord = require('discord.js');
 
+function logError(guildId, error) {
+  console.error(`[${guildId}] ${error}`);
+}
+
+function firestoreEmbedsRef(guildId) {
+  return firestore.collection(`/servers/${guildId}/richEmbeds`);
+}
+
+function databaseEmbedRef(guildId) {
+  return database.ref(guildId);
+}
+
+async function getEmbedData(message, embedId) {
+  if (typeof embedId === 'undefined') {
+    message.channel.send('No ``embed_id`` was given.');
+    return null;
+  }
+
+  const embedDoc = await firestoreEmbedsRef(message.guild.id).doc(embedId).get();
+  if (!embedDoc.exists) {
+    message.channel.send(`No such embed with id ${embedId}.`);
+    return null;
+  }
+  return embedDoc.data();
+}
+
 function help(message) {
   const embed = new Discord.MessageEmbed()
     .setTitle('$embed Help')
@@ -45,17 +71,17 @@ function help(message) {
         inline: false
       },
       {
-        name: 'add',
+        name: 'add [field_name] [args]',
         value: 'Adds a field to the currently edited embed, if it exists.',
         inline: false
       },
       {
-        name: 'update',
+        name: 'update [field_name] [args]',
         value: 'Updates a field in the currently edited embed, if it exists.',
         inline: false
       },
       {
-        name: 'remove',
+        name: 'remove [field_name]',
         value: 'Removes a field from the currently edited embed, if it exists.',
         inline: false
       }
@@ -66,12 +92,13 @@ function help(message) {
 
 // work on making a better list
 async function list(message) {
-  const embedCollection = await firestore.collection(`/servers/${message.guild.id}/announcements`).get();
+  const embedCollection = await firestoreEmbedsRef(message.guild.id).get();
 
   let msg = '';
-  embedCollection.forEach((doc, index) => {
-    console.log(index);
-    msg += `${index+1}.\t${doc.id}\n`;
+  let x = 0;
+  embedCollection.forEach((doc) => {
+    x += 1;
+    msg += `${x}.\t${doc.id}\n`;
   });
 
   message.channel.send(msg);
@@ -84,73 +111,107 @@ async function queue(message, args) {
 
 async function show(message, args) {
   const embedId = args[0];
+  const embedDocData = await getEmbedData(message, embedId);
 
-  if (typeof embedId === 'undefined') {
-    message.channel.send('No ``embed_id`` was given.');
-    return;
-  }
-
-  const embedDoc = await firestore.doc(`/servers/${message.guild.id}/announcements/${embedId}`).get();
-  if (!embedDoc.exists) {
-    message.channel.send(`No such embed with id ${embedId}.`);
-  } else {
-    const embedDocData = embedDoc.data();
-    const embedData = embedDocData.data;
-    message.channel.send({ embed : embedData });
+  if (embedDocData !== null) {
+    message.channel.send({ embed : embedDocData.data })
+      .catch(err => logError(message.guild.id, err));
   }
 }
 
+async function new_(message) {
+
+}
+
 async function edit(message, args) {
-  const snapshot = await database.ref(message.guild.id).once('value');
+  const guildId = message.guild.id;
+
+  const snapshot = await database.ref(guildId).once('value');
   if (snapshot.exists()) {
-    message.channel.send('There is already an embed being edited, save that embed before editing another.');
+    message.channel.send('There is already an embed being edited, save that embed before editing another.')
+      .catch(err => logError(guildId, err));
     return;
   }
 
   const embedId = args[0];
-  if (typeof embedId === 'undefined') {
-    message.channel.send('No ``embed_id`` was given.');
-    return;
-  }
+  const embedDocData = await getEmbedData(message, embedId);
 
-  const embedDoc = await firestore.doc(`/servers/${message.guild.id}/announcements/${embedId}`).get();
-  if (!embedDoc.exists) {
-    message.channel.send(`No such embed with id ${embedId}.`);
-  } else {
-    const embedDocData = embedDoc.data();
-
-    database.ref(message.guild.id).set({
+  if (embedDocData !== null) {
+    database.ref(guildId).set({
       id: embedId,
       ...embedDocData
     }).then(() => {
-      message.channel.send(`Now editing embed with id ${embedId}.`);
+      message.channel.send(`Now editing embed with id ${embedId}.`)
+        .catch(err => logError(guildId, err));
     }).catch((err) => {
-      message.channel.send(JSON.stringify(err));
+      logError(guildId, err);
     });
   }
 }
 
 async function save(message) {
-  const ref = database.ref(message.guild.id);
+  const guildId = message.guild.id;
+  const ref = database.ref(guildId);
 
   const snapshot = await ref.once('value');
   if (!snapshot.exists()) {
-    message.channel.send('There is no embed being edited, try editing an embed before trying to save.');
+    message.channel.send('There is no embed being edited, try editing an embed before trying to save.')
+      .catch(err => logError(guildId, err));
     return;
   }
 
   const edit = snapshot.val();
   
-  // TODO: make this not promise hell
-  firestore.doc(`servers/${message.guild.id}/announcements/${edit.id}`).update({
-    data: edit.data,
-    metadata: edit.metadata
-  }).then(() => {
-    ref.remove();
-    message.channel.send(`Successfully saved ${edit.id}`);
-  }).catch((err) => {
-    message.channel.send(JSON.stringify(err));
-  });
+  try {
+    await firestoreEmbedsRef(guildId).doc(edit.id).set({
+      data: edit.data,
+      metadata: edit.metadata
+    }).catch(err => logError(guildId, err));
+  } catch(err) {
+    logError(guildId, err);
+    return;
+  }
+  await ref.remove()
+    .catch(err => logError(guildId, err));
+  message.channel.send(`Successfully saved embed with id ${edit.id}.`)
+    .catch(err => logError(guildId, err));
+}
+
+async function delete_(message, args) {
+  const embedId = args[0];
+  const guildId = message.guild.id;
+  const userId = message.member.id;
+  const embedDocData = await getEmbedData(message, embedId);
+
+  if (embedDocData !== null) {
+    await message.channel.send(JSON.stringify(embedDocData.metadata), { embed: embedDocData.data })
+      .catch(err => logError(guildId, err));
+    const msg = await message.channel.send('Are you sure you want to delete this embed?')
+      .catch(err => logError(guildId, err));
+
+    await msg.react('✔️').catch(err => logError(guildId, err));
+    await msg.react('❌').catch(err => logError(guildId, err));
+
+    const filter = (reaction, user) => ['✔️',  '❌'].includes(reaction.emoji.name) && user.id == userId;
+    await msg.awaitReactions(filter, { max: 1, time: 15000 }).then(async (collection) => {
+      const reaction = collection.first();
+
+      if (reaction.emoji.name === '✔️') {
+        await firestoreEmbedsRef(guildId).doc(embedId).delete()
+          .catch(err => logError(guildId, err));
+        await msg.channel.send('Embed deleted.')
+          .catch(err => logError(guildId, err));
+      } else {
+        await msg.channel.send('Embed deletion cancelled.')
+          .catch(err => logError(guildId, err));
+      }
+    }).catch(() => {
+      msg.channel.send('No response. Embed deletion cancelled.')
+        .catch(err => logError(guildId, err));
+    });
+    msg.reactions.removeAll()
+      .catch(err => logError(guildId, err));
+  }
 }
 
 module.exports = {
@@ -172,6 +233,7 @@ module.exports = {
         show(message, args);
         break;
       case 'new':
+        new_(message);
         break;
       case 'edit':
         edit(message, args);
@@ -180,6 +242,7 @@ module.exports = {
         save(message);
         break;
       case 'delete':
+        delete_(message, args);
         break;
       case 'add':
         break;
@@ -188,7 +251,8 @@ module.exports = {
       case 'delete':
         break;
       default:
-        message.channel.send('Hmm, that\'s not a command, try $embed help.');
+        message.channel.send('Hmm, that\'s not a command, try $embed help.')
+          .catch(err => logError(guildId, err));
     }
   }
 }
